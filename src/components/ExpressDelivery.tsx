00041fc1966b0d95499e38a38a963a26411a6ba6
Vadim Sokolov <soko1ov.v@mail.ru>
2023-12-06 21:12:48 +0700

    Fixed icons

--- src/components/ExpressDelivery.tsx
+++ src/components/ExpressDelivery.tsx
@@ -1,11 +1,6 @@
 import React, { use, useState } from "react";
+import { ShowBtnIcon } from "./icons/ShowBtnIcon";
 
-const ShowBtnIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 25 15" fill="none">
-    <rect width="25" height="15" rx="4" fill="#CCDBFC" />
-    <rect x="17" y="5" width="5" height="5" rx="2.5" fill="white" />
-    <rect x="10" y="5" width="5" height="5" rx="2.5" fill="white" />
-    <rect x="3" y="5" width="5" height="5" rx="2.5" fill="white" />
-</svg>
 
 export function ExpressDelivery() {
 
